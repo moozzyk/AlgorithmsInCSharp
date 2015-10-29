57967373ac1bb32e7d25088ab946d2857f67e72f
Pawel Kadluczka <moozzyk@gmail.com>
2015-10-28 20:01:44 -0700

    Adding existing code

---
+++ AlgorithmsInCSharp/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //RunSorting();
+            //RunMaximumSubarray();
+            RunMatrixMultiplication();
+        }
+
+        private static void RunSorting()
+        {
+            var inputArray = new[] { 5, 2, 4, 6, 1, 3 };
+            Sorting.InsertionSort(inputArray);
+            Console.WriteLine();
+            Sorting.SelectionSort(inputArray);
+            Console.WriteLine();
+            Sorting.MergeSort(inputArray);
+            Console.WriteLine();
+        }
+
+        private static void RunMaximumSubarray()
+        {
+            var inputArray = new[] { 13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4 , 7 };
+
+            Console.WriteLine("ON3");
+            MaximumSubarray.ON3(inputArray);
+            Console.WriteLine();
+
+            Console.WriteLine("ON2_1");
+            MaximumSubarray.ON2_1(inputArray);
+            Console.WriteLine();
+
+            Console.WriteLine("ON2_2");
+            MaximumSubarray.ON2_2(inputArray);
+            Console.WriteLine();
+
+            Console.WriteLine("NlogN");
+            MaximumSubarray.ONlogN(inputArray);
+            Console.WriteLine();
+
+            Console.WriteLine("N");
+            MaximumSubarray.ON(inputArray);
+            Console.WriteLine();
+
+            Console.WriteLine();
+        }
+
+        private static void RunMatrixMultiplication()
+        {
+            var A = new[,]
+            {
+                {2, 3, 4},
+                {0, 7, 4},
+                {4, 3, 6},
+                {2, 2, 9}
+            };
+
+            var B = new[,]
+            {
+                {3, 9},
+                {5, 2},
+                {8, 1}
+            };
+
+            MatrixMultiplication.ON3(A, B);
+
+            Console.WriteLine();
+
+            //var C = new[,]
+            //{
+            //    {1, 3, 4, 9},
+            //    {7, 5, 6, 1},
+            //    {2, 6, 0, 4},
+            //    {7, 2, 2, 3},
+            //};
+
+            //var D = new[,]
+            //{
+            //    {6, 8, 7, -2},
+            //    {4, 2, 0, 2},
+            //    {-1, 4, 4, 4},
+            //    {2, 6, 3, -4}
+            //};
+
+            var C = new[,]
+            {
+                {1, 3, 4, 9, 1, 4, -5, 6},
+                {7, 5, 6, 1, -9, 0, 2, 3},
+                {2, 6, 0, 4, -4, -5, 1, 7},
+                {7, 2, 2, 3, 4, 2, 3, 3},
+                {5, 1, 5, 3, 3, -3, -4, 1},
+                {3, 2, 2, 1, 9, 3, -3, -3},
+                {9, 3, 1, 1, 7, 4, 5, 5},
+                {8, 8, 8, 4, 1, 2, 5, 9}
+            };
+
+            var D = new[,]
+            {
+                {6, 8, 7, -2, 1, 7, 2, 7},
+                {4, 2, 0, 2, 3, 3, 1, 1},
+                {-1, 4, 4, 4, 3, -4, -3, 5},
+                {2, 6, 3, -4, 1, 5, 0, 4},
+                {9, 2, 1, 1, 1, 7, 2, 7},
+                {1, 3, 3, -4, -9, 0, 2, 3},
+                {8, 3, 2, 3, 9, 4, 5, 9},
+                {6, 2, -5, 2, 3, 1, 1, 8}
+            };
+
+            MatrixMultiplication.ON3(C, D);
+            Console.WriteLine();
+            MatrixMultiplication.RecursiveON3(C, D);
+            Console.WriteLine();
+            MatrixMultiplication.Recursive(C, D);
+
+        }
+    }
+}
---
+++ AlgorithmsInCSharp/Utils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    class Utils
+    {
+        public static void PrintArray<T>(IEnumerable<T> array)
+        {
+            Console.Write("[{0}]", string.Join(", ", array.Select(i => i.ToString())));
+        }
+    }
+}
