133e11fb49106d076f78b675b92b01a35af7142a
moozzyk <moozzyk@gmail.com>
2015-11-24 16:50:53 -0800

    Adding hashtable with open addressing conflict resolution

--- AlgorithmsInCSharp/DataStructures/Hashtable.cs
+++ AlgorithmsInCSharp/DataStructures/Hashtable.cs
@@ -62,6 +62,91 @@ namespace AlgorithmsInCSharp.DataStructures
             var hash = _hashFunction(key);
             return hash % _hashtable.Length;
         }
+    }
 
+    public class Hashtable_OpenAddressing<TKey>
+    {
+        private readonly Func<TKey, int> _hashFunction;
+        private TKey[] _hashtable;
+        private bool[] _wasEverOccupied;
+
+        public TKey[] Hashtable
+        {
+            get { return _hashtable; }
+        }
+
+        public Hashtable_OpenAddressing(int size, Func<TKey, int> hashFunction)
+        {
+            _hashFunction = hashFunction;
+            _hashtable = new TKey[size];
+            _wasEverOccupied = new bool[size];
+        }
+
+        public void Add(TKey key)
+        {
+            if (Equals(key, default(TKey)))
+            {
+                throw new ArgumentException("Cannot use default value of the type as a key.", "key");
+            }
+
+            var index = GetIndex(key, returnFirstEmptySlot: true);
+            if (!index.HasValue)
+            {
+                throw new InvalidOperationException("Hashtable is full.");
+            }
+
+            _hashtable[index.Value] = key;
+
+            // we need to mark the slot as occupied so that we will continue searching for the key
+            // past an empty slot since, due to conflict resolution, the key might be located after empty slots.
+            _wasEverOccupied[index.Value] = true;
+        }
+
+        public TKey Get(TKey key)
+        {
+            if (Equals(key, default(TKey)))
+            {
+                throw new ArgumentException("Cannot use default value of the type as a key.", "key");
+            }
+
+            // due to conflict resolution the item might be located after an empty slot
+            var index = GetIndex(key, returnFirstEmptySlot: false);
+            return index == null ? default(TKey) : _hashtable[index.Value];
+        }
+
+        public bool Delete(TKey key)
+        {
+            if (Equals(key, default(TKey)))
+            {
+                throw new ArgumentException("Cannot use default value of the type as a key.", "key");
+            }
+
+            // due to conflict resolution the item might be located after an empty slot
+            var index = GetIndex(key, returnFirstEmptySlot: false);
+            if (index == null || Equals(_hashtable[index.Value], default(TKey)))
+            {
+                return false;
+            }
+
+            _hashtable[index.Value] = default(TKey);
+
+            return true;
+        }
+
+        private int? GetIndex(TKey key, bool returnFirstEmptySlot)
+        {
+            var hash = _hashFunction(key);
+            var index =  hash % _hashtable.Length;
+
+            for (var i = index; i != (_hashtable.Length + index - 1) % _hashtable.Length; i = (i + 1) % _hashtable.Length)
+            {
+                if (key.Equals(_hashtable[i]) || (_hashtable[i] == null && (returnFirstEmptySlot || !_wasEverOccupied[i])))
+                {
+                    return i;
+                }
+            }
+
+            return null;
+        }
     }
 }
--- AlgorithmsInCSharp/HashtableRunner.cs
+++ AlgorithmsInCSharp/HashtableRunner.cs
@@ -8,36 +8,110 @@ namespace AlgorithmsInCSharp
     {
         public static void Run()
         {
-             Utils.PrintAlgorithmRun("Hashtable - chaining collision resolution", RunHashtableChaining);
-        }
-
-        private static void RunHashtableChaining()
-        {
-            var hashtable = new Hashtable_Chaining<string>(3, DivisionHashFunction);
-            var keys = new[] { "123", "abc", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "1234567890"};
+            int size = 3;
+            var keys = new[] { "123", "abc", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "1234567890", "123"};
             var nonExistingKey = "~!@#$%^";
 
-            foreach (var key in keys)
+             Utils.PrintAlgorithmRun(
+                 "Hashtable - chaining collision resolution", () => RunHashtable_Chaining.Run(size, keys, nonExistingKey));
+
+             Utils.PrintAlgorithmRun(
+                 "Hashtable - open addressing resolution", () => RunHashtable_OpenAddressing.Run(size, keys, nonExistingKey));
+        }
+
+        private static class RunHashtable_Chaining
+        {
+            public static void Run(int size, string[] keys, string nonExistingKey)
             {
-                Console.WriteLine($"Adding: {key}, hash value: {DivisionHashFunction(key)}");
-                hashtable.Add(key);
+                var hashtable = new Hashtable_Chaining<string>(size, DivisionHashFunction);
+
+                foreach (var key in keys)
+                {
+                    Console.WriteLine($"Adding: {key}, hash value: {DivisionHashFunction(key)}");
+                    hashtable.Add(key);
+                }
+                Console.WriteLine();
+
+                PrintHashtable(hashtable);
+
+                foreach (var key in keys.Concat(new[] { nonExistingKey }))
+                {
+                    Console.WriteLine($"Retrieving key: {key}. Result: {hashtable.Get(key) == key}");
+                }
+                Console.WriteLine();
+
+                Console.WriteLine($"Deleting {keys[0]}. Result {hashtable.Delete(keys[0])}");
+                Console.WriteLine($"Deleting {keys[2]}. Result {hashtable.Delete(keys[2])}");
+                Console.WriteLine($"Deleting {nonExistingKey}. Result {hashtable.Delete(nonExistingKey)}");
+                Console.WriteLine();
+
+                PrintHashtable(hashtable);
             }
-            Console.WriteLine();
 
-            PrintHashtable(hashtable);
-
-            foreach (var key in keys.Concat(new[] { nonExistingKey }))
+            private static void PrintHashtable<T>(Hashtable_Chaining<T> hashtable)
             {
-                Console.WriteLine($"Retrieving key: {key}. Result: {hashtable.Get(key) == key}");
+                Console.WriteLine("Hashtable contents: ");
+                foreach (var list in hashtable.Hashtable)
+                {
+                    Utils.PrintList<T>(list.Head);
+                }
+                Console.WriteLine();
             }
-            Console.WriteLine();
+        }
 
-            Console.WriteLine($"Deleting {keys[0]}. Result {hashtable.Delete(keys[0])}");
-            Console.WriteLine($"Deleting {keys[2]}. Result {hashtable.Delete(keys[2])}");
-            Console.WriteLine($"Deleting {nonExistingKey}. Result {hashtable.Delete(nonExistingKey)}");
-            Console.WriteLine();
+        private static class RunHashtable_OpenAddressing
+        {
+            public static void Run(int size, string[] keys, string nonExistingKey)
+            {
+                var hashtable = new Hashtable_OpenAddressing<string>(size, DivisionHashFunction);
 
-            PrintHashtable(hashtable);
+                foreach (var key in keys)
+                {
+                    Console.WriteLine($"Adding: {key}, hash value: {DivisionHashFunction(key)}");
+                    try
+                    {
+                        hashtable.Add(key);
+                    }
+                    catch (InvalidOperationException ex)
+                    {
+                        Console.WriteLine($"Failed to add {key}. Exception: {ex.Message}");
+                    }
+                }
+                Console.WriteLine();
+
+                PrintHashtable(hashtable);
+
+                foreach (var key in keys.Concat(new[] { nonExistingKey }))
+                {
+                    Console.WriteLine($"Retrieving key: {key}. Result: {hashtable.Get(key) == key}");
+                }
+                Console.WriteLine();
+
+                Console.WriteLine($"Deleting {keys[0]}. Result {hashtable.Delete(keys[0])}");
+                Console.WriteLine($"Deleting {keys[2]}. Result {hashtable.Delete(keys[2])}");
+                Console.WriteLine($"Deleting {nonExistingKey}. Result {hashtable.Delete(nonExistingKey)}");
+                Console.WriteLine();
+
+                PrintHashtable(hashtable);
+
+                foreach (var key in keys.Concat(new[] { nonExistingKey }))
+                {
+                    Console.WriteLine($"Retrieving key: {key}. Result: {hashtable.Get(key) == key}");
+                }
+                Console.WriteLine();
+            }
+
+            private static void PrintHashtable<T>(Hashtable_OpenAddressing<T> hashtable)
+            {
+                Console.WriteLine("Hashtable contents: ");
+
+                Console.WriteLine(
+                    string.Join(", ",
+                        hashtable.Hashtable.Select(
+                            k => string.Format("{0}{1}", k, Equals(k, default(T)) ? "(nil)" : string.Empty))));
+
+                Console.WriteLine();
+            }
         }
 
         private static int DivisionHashFunction(string key)
@@ -49,16 +123,6 @@ namespace AlgorithmsInCSharp
             }
 
             return hash;
-        }
-
-        private static void PrintHashtable<T>(Hashtable_Chaining<T> hashtable)
-        {
-            Console.WriteLine("Hashtable contents: ");
-            foreach (var list in hashtable.Hashtable)
-            {
-                Utils.PrintList<T>(list.Head);
-            }
-            Console.WriteLine();
         }
     }
 }
