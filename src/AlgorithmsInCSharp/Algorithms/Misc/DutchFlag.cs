c53169005657efe9656cc5a502f99c2d5a8e72f8
moozzyk <moozzyk@gmail.com>
2016-01-11 20:46:27 -0800

    Adding Dutch National Flag

---
+++ src/AlgorithmsInCSharp/Algorithms/Misc/DutchFlag.cs
@@ -0,0 +1,48 @@
+namespace AlgorithmsInCSharp.Algorithms.Misc
+{
+    public class DutchFlag
+    {
+        public static void Partition(int[] array, int pivotIdx)
+        {
+            var pivot = array[pivotIdx];
+            var firstPartitionEnd = 0;
+            var secondPartitionEnd = 0;
+            var thirdPartitionStart = array.Length - 1;
+
+            while (secondPartitionEnd <= thirdPartitionStart)
+            {
+                if (array[secondPartitionEnd] < pivot)
+                {
+                    swap(array, firstPartitionEnd, secondPartitionEnd);
+                    secondPartitionEnd++;
+                    firstPartitionEnd++;
+                }
+                else if (array[secondPartitionEnd] > pivot)
+                {
+                    swap(array, secondPartitionEnd, thirdPartitionStart);
+                    thirdPartitionStart--;
+                }
+                else
+                {
+                    secondPartitionEnd++;
+                }
+            }
+        }
+
+        private static void swap(int[] array, int i, int j)
+        {
+            var tmp = array[i];
+            array[i] = array[j];
+            array[j] = tmp;
+        }
+    }
+}
+
+/*
+7
+
+                             v
+  2, 5, 4, 4, 1, 2, 6, 7, 9, 3, 8
+                    ^           ^
+*/
+
--- src/AlgorithmsInCSharp/MiscAlgorithmsRunner.cs
+++ src/AlgorithmsInCSharp/MiscAlgorithmsRunner.cs
@@ -8,15 +8,25 @@ namespace AlgorithmsInCSharp
         public static void Run()
         {
             Utils.PrintAlgorithmRun("QuickSelect", RunQuickSelect);
+            Utils.PrintAlgorithmRun("DutchFlag", RunDutchFlag);
         }
 
         private static void RunQuickSelect()
         {
-            var inputArray = new[] { 5, 8, 7, 2, 6, 3, 9};
+            var inputArray = new[] { 5, 8, 7, 2, 6, 3, 9 };
             Console.WriteLine($"{Utils.FormatArray(inputArray)}");
             Console.WriteLine($"Min: {QuickSelect.Select(inputArray, 0)}");
             Console.WriteLine($"Max: {QuickSelect.Select(inputArray, inputArray.Length - 1)}");
             Console.WriteLine($"4th smallest (0 based): {QuickSelect.Select(inputArray, 4)}");
+        }
+
+        private static void RunDutchFlag()
+        {
+            var inputArray = new[] { 2, 5, 8, 4, 4, 7, 1, 2, 6, 3, 9 };
+            int pivotIdx = 4;
+            Console.WriteLine($"Input: {Utils.FormatArray(inputArray)}, pivot: {inputArray[pivotIdx]}");
+            DutchFlag.Partition(inputArray, pivotIdx);
+            Console.WriteLine($"{Utils.FormatArray(inputArray)}");
         }
     }
 }
